8ef9511a0ce064e2f3399e82c87a654824c9afac
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-07-06 22:18:10 +0500

    Set up codebase, install packages and collect assets.

---
+++ lib/models/content.model.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Content {
+  final String name;
+  final String title;
+  final String imageUrl;
+  final String videoUrl;
+  final String description;
+  final Color color;
+
+  Content(
+    this.name,
+    this.title,
+    this.imageUrl,
+    this.videoUrl,
+    this.description,
+    this.color,
+  );
+}
