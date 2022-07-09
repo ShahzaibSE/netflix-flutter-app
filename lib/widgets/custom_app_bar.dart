bd7b5ab4ebb310273507786ce6567ed53fd74835
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-07-09 18:54:07 +0500

    Added App Bar Button widget for reusability

--- lib/widgets/custom_app_bar.dart
+++ lib/widgets/custom_app_bar.dart
@@ -1,3 +1,5 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:netflix_flutter_app/assets.dart';
 
@@ -9,10 +11,40 @@ class CustomAppBar extends StatelessWidget {
     return Container(
       color: Colors.orange,
       padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 24.0),
-      child: Row(
-        children: [
-          Image.asset(Assets.netflixLogo0),
-        ],
+      child: SafeArea(
+        child: Row(
+          children: [
+            Image.asset(Assets.netflixLogo0),
+            const SizedBox(width: 12.0),
+            Row(
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _AppBarButton extends StatelessWidget {
+  // const _AppBarButton({Key? key}) : super(key: key);
+  final String title;
+  final Function() onTap;
+
+  const _AppBarButton(Key key, {required this.title, required this.onTap})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16.0,
+          fontWeight: FontWeight.w600,
+        ),
       ),
     );
   }
