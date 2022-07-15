2fbb85e47a17bc9271ab2c8aaec0930e7338074f
Shahzaib Noor <shahzaibnoorcs@gmail.com>
2022-07-15 17:31:22 +0500

    Creating custom scroll view

--- lib/screens/home.screen.dart
+++ lib/screens/home.screen.dart
@@ -9,6 +9,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  late ScrollController _scrollController;
+  double _scrollOffset = 0.0;
+
+  void initState() {
+    _scrollController = ScrollController()
+      ..addListener(() {
+        setState(() {
+          _scrollOffset = _scrollController.offset;
+        });
+      });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
@@ -17,9 +36,21 @@ class _HomeScreenState extends State<HomeScreen> {
         child: CustomAppBar(),
         preferredSize: Size(screenSize.width, 50.0),
       ),
+      body: CustomScrollView(
+        controller: _scrollController,
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              height: 1000.0,
+              color: Colors.blue,
+            ),
+          ),
+        ],
+      ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.grey[850],
         onPressed: () => {print('Cast')},
+        child: const Icon(Icons.cast),
       ),
     );
   }
